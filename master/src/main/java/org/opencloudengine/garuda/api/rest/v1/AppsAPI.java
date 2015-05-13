e95e58abacfe81391ef3ebf8513b011eb0e9411f
Sang Song <swsong@fastcat.co>
2015-05-13 10:18:57 +0900

    app을 생성하고, 시작시키는 기반마련.

--- master/src/main/java/org/opencloudengine/garuda/api/rest/v1/AppsAPI.java
+++ master/src/main/java/org/opencloudengine/garuda/api/rest/v1/AppsAPI.java
@@ -2,7 +2,6 @@ package org.opencloudengine.garuda.api.rest.v1;
 
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -10,26 +9,38 @@ import javax.ws.rs.core.Response;
 import java.io.*;
 
 /**
- * File Upload API
+ * Deploy App API
  *
  * @author Sang Wook, Song
  *
  */
-@Path("/v1/deploy")
-public class DeployPhpAPI {
+@Path("/v1/apps")
+public class AppsAPI {
 
 	private static final String SERVER_UPLOAD_LOCATION_FOLDER = "/tmp";
 
 	/**
-	 * Upload a File
-	 */
+	 * Create apps.
+	 * Docker 이미지를 만들어서 Registry에 등록한다.
+	 *
+	 * POST /v1/apps
+	 *
+	 * 결과 :
+	 * {
+	 *     "appId": "...",
+	 *     "status" : "complete",
+	 *     "type": "create"
+	 * }
+	 * */
 	@POST
-	@Path("php")
+	@Path("/")
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
-	public Response uploadFile(
-			@FormDataParam("appFile") InputStream fileInputStream,
-			@FormDataParam("appFile") FormDataContentDisposition contentDispositionHeader,
-			@RequestParam int instanceSize, @RequestParam int port, @RequestParam String userId) {
+	public Response createApp(
+			@FormDataParam("instanceSize") int instanceSize,
+			@FormDataParam("port") int port,
+			@FormDataParam("userId") String userId,
+			@FormDataParam("file") InputStream fileInputStream,
+			@FormDataParam("file") FormDataContentDisposition contentDispositionHeader) {
 
 		String filePath = SERVER_UPLOAD_LOCATION_FOLDER	+ contentDispositionHeader.getFileName();
 
@@ -38,10 +49,11 @@ public class DeployPhpAPI {
 
 		String output = "File saved to server location : " + filePath;
 
-		//TODO 1 deploy_php_apache.sh aaa.zip <userId>_<appFile name>_php <registry addr> "php5_apache2"
+		//TODO deploy_php_apache.sh aaa.zip <userId>_<appFile name>_php <registry addr> "php5_apache2"
 
 
-		//TODO 2. 짠 코드. 마라톤 디플로이.
+		//TODO 결과에 appId가 반들시 포함되어야 run, delete 등을 수행할수 있다.
+		//TODO appid는 userId-imageId 로 자동생성한다.
 
 		return Response.ok(output).build();
 
@@ -69,11 +81,28 @@ public class DeployPhpAPI {
 
 	}
 
+	/**
+	 * Run App
+	 *
+	 * POST /v1/apps/$APP_ID/run
+	 *
+	 * 결과 :
+	 * {
+	 *     "appId": "...",
+	 *     "status" : "in-progress",
+	 *     "type": "run"
+	 * }
+	 * */
 	@GET
-	@Path("test")
+	@Path("{appID}/run")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String test() {
-		return "Test";
+	public Response runApp(@PathParam("appId") String appId) {
+
+		/*
+		* TODO 짠 코드. 마라톤 디플로이.
+		* */
+
+		return Response.ok("").build();
 	}
 
 }
