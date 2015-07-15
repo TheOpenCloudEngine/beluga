096e9ed8cffa055a6b0ac14e3937538964ef29ed
Sang Song <swsong@fastcat.co>
2015-07-15 13:33:18 +0900

    test

--- master/src/test/java/org/opencloudengine/garuda/cloud/controller/EC2JCloudTest.java
+++ master/src/test/java/org/opencloudengine/garuda/cloud/controller/EC2JCloudTest.java
@@ -55,12 +55,11 @@ public class EC2JCloudTest {
     ComputeService compute;
     Template template;
 
-    public EC2JCloudTest(String accessKey, String secretKey, String group) {
-
+    public EC2JCloudTest(String accessKey, String secretKey) {
         init(accessKey, secretKey);
     }
 
-    public void init(String accessKey, String secretKey) {
+    private void init(String accessKey, String secretKey) {
         Properties overrides = new Properties();
         context = ContextBuilder.newBuilder("aws-ec2")
                 .credentials(accessKey, secretKey)
@@ -71,138 +70,32 @@ public class EC2JCloudTest {
         compute = context.getComputeService();
     }
 
-    public void initTemplate(String instanceType, String imageId, String group) {
+    public void initTemplate(String instanceType, String imageId, String group, String keyPair) {
         TemplateBuilder templateBuilder = compute.templateBuilder();
         templateBuilder.hardwareId(instanceType); //InstanceType.T2_MICRO
         templateBuilder.imageId(imageId); //"ap-northeast-1/ami-936d9d93"
+
         template = templateBuilder.build();
         // specify your own groups which already have the correct rules applied
-        template.getOptions().as(AWSEC2TemplateOptions.class).securityGroups(group);
+        template.getOptions().as(AWSEC2TemplateOptions.class).securityGroups(group).keyPair(keyPair);
     }
 
     public Set<? extends NodeMetadata> launchInstance(String prefixId, int scale) throws RunNodesException {
         return compute.createNodesInGroup(prefixId, scale, template);
     }
 
+    public void close() {
+        context.close();
+    }
 
     public static void main(String[] args) throws RunNodesException {
+        String accessKey = args[0];
+        String secretKey = args[1];
 
-//        if (args.length < PARAMETERS)
-//            throw new IllegalArgumentException(INVALID_SYNTAX);
-
-        // Args
-        String accesskeyid = args[0];
-        String secretkey = args[1];
-        String group = "default";//args[2];
-
-        // Init
-        Properties overrides = new Properties();
-// choose only amazon images that are ebs-backed
-//        overrides.setProperty(AWSEC2Constants.PROPERTY_EC2_AMI_QUERY,
-//                "owner-id=137112412989;state=available;image-type=machine;root-device-type=ebs");
-        ComputeServiceContext context = ContextBuilder.newBuilder("aws-ec2")
-                .credentials(accesskeyid, secretkey)
-                .overrides(overrides)
-                .modules(ImmutableSet.<Module> of(new SLF4JLoggingModule(),
-                        new SshjSshClientModule()))
-                .buildView(ComputeServiceContext.class);
-
-        ComputeService compute = context.getComputeService();
-//        ComputeService compute = new ComputeServiceContextFactory().createContext("aws-ec2", accesskeyid, secretkey)
-//                .getComputeService();
-
-
-
-
-        // here's an example of the portable api
-//        Set<? extends Location> locations = compute.listAssignableLocations();
-
-//        Set<? extends Image> images = compute.listImages();
-//for(Image i : images) {
-//    String loc = i.getLocation().getId();
-//    if(loc.startsWith("ap-northeast-1")) {
-//
-//        System.out.println(i);
-//    }
-//    String fa = i.getOperatingSystem().getFamily().value();
-//    if(fa.contains("ubuntu")) {
-//        System.out.println(" UBUNTU >>>>>>>>>" + i);
-//    }
-//}
-// pick the highest version of the RightScale CentOS template
-        TemplateBuilder templateBuilder = compute.templateBuilder();
-        templateBuilder.hardwareId(InstanceType.T2_MICRO);
-//        templateBuilder.osVersionMatches("14.04");
-//        templateBuilder.osFamily(OsFamily.UBUNTU);
-        templateBuilder.imageId("ap-northeast-1/ami-936d9d93");
-//        templateBuilder.locationId("ap-northeast-1");
-        Template template = templateBuilder.build();
-
-
-
-// specify your own groups which already have the correct rules applied
-        template.getOptions().as(AWSEC2TemplateOptions.class).securityGroups(group);
-
-// specify your own keypair for use in creating nodes
-//        template.getOptions().as(AWSEC2TemplateOptions.class).keyPair(keyPair);
-
-// run a couple nodes accessible via group
-        Set<? extends NodeMetadata> nodes = compute.createNodesInGroup("webserver", 2, template);
-
-// when you need access to very ec2-specific features, use the provider-specific context
-//        AWSEC2Client ec2Client = AWSEC2Client.class.cast(context.getProviderSpecificContext().getApi());
-
-// ex. to get an ip and associate it with a node
-//        NodeMetadata node = Iterables.get(nodes, 0);
-//        String ip = ec2Client.getElasticIPAddressServices().allocateAddressInRegion(node.getLocation().getId());
-//        ec2Client.getElasticIPAddressServices().associateAddressInRegion(node.getLocation().getId(), ip, node.getProviderId());
-
-        context.close();
-
-
-
-        // wait up to 60 seconds for ssh to be accessible
-//        try {
-//            if (command.equals("create")) {
-//
-////                Template template = compute.templateBuilder().build();
-//
-//                template.getOptions().as(AWSEC2TemplateOptions.class)
-//                        // set the price as 3 cents/hr
-//                        .spotPrice(0.03f)
-//                                // authorize my ssh key
-//                        .authorizePublicKey(
-//                                Files.toString(new File(System.getProperty("user.home") + "/.ssh/id_rsa.pub"),
-//                                        Charsets.UTF_8));
-//
-//                System.out.printf(">> running one spot node type(%s) with ami(%s) in group(%s)%n", template.getHardware()
-//                        .getProviderId(), template.getImage().getId(), group);
-//                // run only a single node
-//                NodeMetadata node = Iterables.getOnlyElement(compute.createNodesInGroup(group, 1, template));
-//
-//                System.out.printf("<< running node(%s)%n", node.getId());
-//
-//            } else if (command.equals("destroy")) {
-//                System.out.printf(">> destroying nodes in group(%s)%n", group);
-//                Set<? extends NodeMetadata> destroyed = compute.destroyNodesMatching(NodePredicates.inGroup(group));
-//                System.out.printf("<< destroyed(%d)%n", destroyed.size());
-//                System.exit(0);
-//            } else {
-//                System.err.println(INVALID_SYNTAX);
-//                System.exit(1);
-//            }
-//        } catch (RunNodesException e) {
-//            System.err.println(e.getMessage());
-//            for (NodeMetadata node : e.getNodeErrors().keySet())
-//                compute.destroyNode(node.getId());
-//            System.exit(1);
-//        } catch (IOException e) {
-//            System.err.println(e.getMessage());
-//            System.exit(1);
-//        } finally {
-//            compute.getContext().close();
-//        }
-
+        EC2JCloudTest test = new EC2JCloudTest(accessKey, secretKey);
+        test.initTemplate(InstanceType.T2_MICRO, "ap-northeast-1/ami-936d9d93", "default", "aws-garuda");
+        test.launchInstance("test", 1);
+        test.close();
     }
 
 }
