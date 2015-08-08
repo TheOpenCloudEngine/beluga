577417bd1f2a4509bd8b4a7d9176b0ffcde07de2
Sang Song <swsong@fastcat.co>
2015-08-09 08:22:04 +0900

    .

--- master/src/test/java/org/opencloudengine/garuda/mesos/marathon/MarathonAPITest.java
+++ master/src/test/java/org/opencloudengine/garuda/mesos/marathon/MarathonAPITest.java
@@ -1,7 +1,5 @@
 package org.opencloudengine.garuda.mesos.marathon;
 
-import com.amazonaws.util.json.JSONException;
-import com.amazonaws.util.json.JSONObject;
 import org.junit.Test;
 import org.opencloudengine.garuda.mesos.marathon.message.GetApp;
 import org.opencloudengine.garuda.mesos.marathon.message.GetApps;
@@ -12,10 +10,8 @@ import org.slf4j.LoggerFactory;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by swsong on 2015. 8. 9..
@@ -30,7 +26,7 @@ public class MarathonAPITest {
     @Test
     public void getApps() {
         Client client = ClientBuilder.newClient();
-        String path = "v2/apps";
+        String path = "/v2/apps";
 
         GetApps apps = client.target(target).path(path).request(MediaType.APPLICATION_JSON_TYPE).get(GetApps.class);
         List<App> appList = apps.getApps();
@@ -49,8 +45,8 @@ public class MarathonAPITest {
         Client client = ClientBuilder.newClient();
         String path = "/v2/apps/"+appId;
 
-        Map<String, App> getApp = client.target(target).path(path).request().accept(MediaType.APPLICATION_JSON_TYPE).get(new GenericType<Map<String, App>>(){});
-        App app = getApp.get("app");
+        GetApp getApp = client.target(target).path(path).request(MediaType.APPLICATION_JSON_TYPE).get(GetApp.class);
+        App app = getApp.getApp();
         System.out.println(app);
         System.out.println("id : " + app.getId());
         System.out.println("cmd : " + app.getCmd());
@@ -60,19 +56,19 @@ public class MarathonAPITest {
 
     @Test
     public void deployApp() {
-        String appId = "nc3";
+        String appId = "nc5";
         Client client = ClientBuilder.newClient();
         String path = "/v2/apps";
-        String cmd  = "";
+        String cmd  = "while true; do { echo -e \"HTTP/1.1 200 OK\\r\\n$(date)\\r\\n\\r\\n\"; echo \"Hello~~\"; } | nc -l $PORT; done";
         App app = new App(appId);
         app.setCmd(cmd);
         app.setCpus(0.1f);
         app.setMem(16.0f);
-        app.setInstances(1);
-        logger.debug("data : {}", Entity.json(app).toString());
+        app.setInstances(3);
 
-        App appResponse = client.target(target).path(path).request(MediaType.APPLICATION_JSON_TYPE).accept(MediaType.APPLICATION_JSON_TYPE).post(Entity.json(app), App.class);
+        App appResponse = client.target(target).path(path).request(MediaType.APPLICATION_JSON_TYPE).post(Entity.json(app), App.class);
         System.out.println(appResponse);
+
         System.out.println("id : " + appResponse.getId());
         System.out.println("cmd : " + appResponse.getCmd());
         logger.info("additional : ", appResponse.getAdditionalProperties());
