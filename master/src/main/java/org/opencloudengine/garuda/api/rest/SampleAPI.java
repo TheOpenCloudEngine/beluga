cef3ae04126fbca7b425226245a70a1a86cefef3
Sang Song <swsong@fastcat.co>
2015-08-10 13:23:38 +0900

    파일업로드 테스트.

--- master/src/main/java/org/opencloudengine/garuda/api/rest/SampleAPI.java
+++ master/src/main/java/org/opencloudengine/garuda/api/rest/SampleAPI.java
@@ -1,9 +1,12 @@
 package org.opencloudengine.garuda.api.rest;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.*;
 
 /**
  * Sample
@@ -21,4 +24,47 @@ public class SampleAPI {
 		return "Test";
 	}
 
+    @POST
+    @Path("/fileupload")  //Your Path or URL to call this service
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response uploadFile(
+            @FormDataParam("file") InputStream uploadedInputStream,
+            @FormDataParam("file") FormDataContentDisposition fileDetail) {
+        //Your local disk path where you want to store the file
+        String uploadedFileLocation = "/tmp/" + fileDetail.getFileName();
+        System.out.println(uploadedFileLocation);
+        // save it
+        File objFile=new File(uploadedFileLocation);
+        if(objFile.exists())
+        {
+            objFile.delete();
+
+        }
+
+        saveToFile(uploadedInputStream, uploadedFileLocation);
+
+        String output = "File uploaded via Jersey based RESTFul Webservice to: " + uploadedFileLocation;
+
+        return Response.status(200).entity(output).build();
+
+    }
+    private void saveToFile(InputStream uploadedInputStream, String uploadedFileLocation) {
+
+        try {
+            OutputStream out = null;
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            out = new FileOutputStream(new File(uploadedFileLocation));
+            while ((read = uploadedInputStream.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+    }
 }
