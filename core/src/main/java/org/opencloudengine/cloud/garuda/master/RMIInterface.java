749e3695c3f25f20ee50e716ad2b3badfe3d3b52
sangwook.song <swsong@fastcat.co>
2015-08-24 01:10:16 +0900

    v1.1

--- core/src/main/java/org/opencloudengine/cloud/garuda/master/RMIInterface.java
+++
@@ -1,9 +0,0 @@
-package org.opencloudengine.cloud.garuda.master;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface RMIInterface extends Remote {
-	//void update(String string, ArrayList<SessionObject> appSessionList) throws RemoteException;
-	void update(String string) throws RemoteException;
-}
