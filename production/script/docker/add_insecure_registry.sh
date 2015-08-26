8c3408e48c68fd13b9747d78540b186b650f2a9b
Sang Song <swsong@fastcat.co>
2015-08-27 04:09:54 +0900

    도커 레지스트리 주소를 추가하는 스크립트 개발.

---
+++ production/script/docker/add_insecure_registry.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# 접근할 레지스트리 주소를 도커 설정에 추가한다.
+# @author : Sang Wook, Song
+#
+
+if [ $# -ne 1 ] ; then
+    echo "Please put registry server address."
+    exit 1
+fi
+
+echo DOCKER_OPTS=\"\$DOCKER_OPTS --insecure-registry $1:5000\" | sudo tee -a /etc/default/docker && sudo /etc/init.d/docker restart
